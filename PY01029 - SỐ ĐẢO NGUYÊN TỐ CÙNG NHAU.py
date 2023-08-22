d2af416151a1ea1b78f6d8bc5f164e9a67ddaa82
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-08-22 19:02:40 +0700

    Create PY01029 - SỐ ĐẢO NGUYÊN TỐ CÙNG NHAU.py

---
+++ PY01029 - SỐ ĐẢO NGUYÊN TỐ CÙNG NHAU.py
@@ -0,0 +1,12 @@
+import math
+
+t = int(input())
+
+while t > 0:
+    t -= 1
+    a = input()
+    b = a[::-1]
+    if math.gcd(int(a), int(b)) == 1:
+        print("YES")
+    else:
+        print("NO")
