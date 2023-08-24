c2b3c621fdff463fbd621b7bc77c8b3f3a05460d
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-08-24 09:07:21 +0700

    Create PY01059 - Tổng chữ số - tích chữ số.py

---
+++ PY01059 - Tổng chữ số - tích chữ số.py
@@ -0,0 +1,20 @@
+import math
+
+t = int(input())
+
+while t > 0:
+    t -= 1
+    n = input()
+    sum = 0
+    tich = 1
+    cnt = 0
+    for i in range(len(n)):
+        if i % 2 == 0:
+            sum += int(n[i])
+        else:
+            if n[i] != "0":
+                cnt = 1
+                tich *= int(n[i])
+    if cnt == 0:
+        tich = 0
+    print(str(sum) + " " + str(tich))   
