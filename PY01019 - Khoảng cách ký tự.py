ae49ead6e8a5385c5db6597f4d42c9ab559dfb23
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-08-27 21:49:52 +0700

    Create PY01019 - Khoảng cách ký tự.py

---
+++ PY01019 - Khoảng cách ký tự.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+while t > 0:
+    t -= 1
+    s1 = input()
+    s2 = s1[::-1]
+    check = 0
+    for i in range(1, len(s1)):
+        if abs(ord(s1[i]) - ord(s1[i-1])) != abs(ord(s2[i]) - ord(s2[i-1])):
+            check = 1
+            break
+    if check == 0:
+        print("YES")
+    else:
+        print("NO")
+    
