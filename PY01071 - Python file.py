195fb45458010929d1c443558b2b82e45d1b5823
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-08-27 21:38:39 +0700

    Create PY01071 - Python file.py

---
+++ PY01071 - Python file.py
@@ -0,0 +1,17 @@
+def check(s):
+    if len(s) < 3:
+        return False
+    s = s.lower()
+    if s[len(s) - 3:] != '.py':
+        return False
+    for i in s:
+        if ord(i) < ord('a') and ord(i) > ord('z') and ord(i) != ord('.') and ord(i) != ord('_'):
+            return False
+    return True
+
+s = input()
+
+if check(s):
+    print("yes")
+else:
+    print("no")
