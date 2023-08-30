24f3e6f66e71d49c5dc6861ac0acb1f302d63b5b
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-08-30 09:24:27 +0700

    Create PY02020 - Tính điểm trung bình.py

---
+++ PY02020 - Tính điểm trung bình.py
@@ -0,0 +1,27 @@
+import math
+
+n = int(input())
+a = [float(i) for i in input().split()]
+a.sort()
+
+l = 0
+r = 0
+
+for i in range(1, len(a)):
+    if a[i-1] != a[i]:
+        l = i
+        break
+
+for i in range(1, len(a)):
+    if a[len(a) - i] != a[len(a) - i - 1]:
+        r = len(a) - 1
+        break
+    
+sum = 0
+cnt = 0
+for i in range(l, r):
+    sum += a[i]
+    cnt += 1
+
+print("{:.2f}".format(sum / cnt))
+    
