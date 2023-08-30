80e1460070528ff8939ab1b3b5a41045952ef325
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-08-30 13:54:47 +0700

    Create PY02050 - Đoạn liên tiếp nhỏ hơn.py

---
+++ PY02050 - Đoạn liên tiếp nhỏ nhất.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+while t > 0:
+    t -= 1
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    st = []
+    for i in range(n):
+        while len(st) > 0 and a[st[-1]] <= a[i]: st.pop()
+        if len(st) == 0: print(i + 1, end = " ")
+        else: print(i - st[-1], end = " ")
+        st.append(i)
+    print()
