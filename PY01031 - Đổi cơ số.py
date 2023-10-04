34ae041504a0e492a818a1b4fb4b2f49d3480246
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-10-04 07:39:52 +0700

    Create PY01031 - Đổi cơ số.py

---
+++ PY01031 - Đổi cơ số.py
@@ -0,0 +1,13 @@
+P = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+t = int(input())
+
+while t > 0:
+    t -= 1
+    n, b = list(map(int, input().split()))
+    s = ''
+    while n > 0:
+        tmp = n % b
+        s = P[tmp] + s
+        n //= b
+    print(s)
