bbedc8e8316f328b3a322959a0c33e4e3bd9c861
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-11-20 15:41:59 +0700

    Create PY01024 - Sắp xếp theo tích chữ số.py

---
+++ PY01024 - Sắp xếp theo tích chữ số.py
@@ -0,0 +1,22 @@
+import functools
+
+def cnt(n):
+    s = 1
+    while n != 0:
+        s *= n % 10
+        n //= 10
+    return s
+
+def cmp(a, b):
+    if cnt(a) == cnt(b):
+        if a > b: return 1
+        else: return -1
+    elif cnt(a) > cnt(b): return 1
+    else: return -1
+
+for t in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a = sorted(a, key = functools.cmp_to_key(cmp))
+    for i in a: print(i, end = " ")
+    print()
