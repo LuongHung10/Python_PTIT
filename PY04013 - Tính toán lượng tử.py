3526145114f640561dd1b2881e2fc212fc2fdcbe
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-12-15 17:03:18 +0700

    Create PY04013 - Tính toán lượng tử.py

---
+++ PY04013 - Tính toán lượng tử.py
@@ -0,0 +1,21 @@
+def time(x, y):
+    return y[0]*60 + y[1] - x[0]*60 - x[1]
+
+
+a = {}
+t = int(input())
+for i in range(t):
+    ten = input()
+    x = [int(i) for i in input().split(':')]
+    y = [int(i) for i in input().split(':')]
+    luongMua = int(input())
+    if ten not in a:
+        a[ten] = (time(x, y), luongMua)
+    else:
+        a[ten] = (a[ten][0] + time(x, y), a[ten][1] + luongMua)
+        
+
+p = 1
+for i in a:
+    print('T{:02d}'.format(p), i, "{:.2f}".format(a[i][1] * 60/a[i][0]))
+    p += 1
